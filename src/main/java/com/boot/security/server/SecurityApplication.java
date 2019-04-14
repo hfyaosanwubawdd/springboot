6fc3737b1e7821a62e6dfcaf61a9a70c61c15eca
Administrator <Administrator@SC-201902250200>
2019-04-14 17:35:39 +0800

    111

---
+++ src/main/java/com/boot/security/server/SecurityApplication.java
@@ -0,0 +1,18 @@
+package com.boot.security.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * 
+ * @author 小威老师 xiaoweijiagou@163.com
+ *
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+}
