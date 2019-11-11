2ac359d4ba51266219fa9ff06b7c7a47d4b9ad90
Administrator <Administrator@SC-201903281920>
2019-11-11 18:16:19 +0800

    123

--- src/main/java/com/boot/security/server/service/impl/IpsInfoServiceImpl.java
+++ src/main/java/com/boot/security/server/service/impl/IpsInfoServiceImpl.java
@@ -1,4 +1,6 @@
 package com.boot.security.server.service.impl;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,13 +9,21 @@ import com.boot.security.server.model.IpsInfoEntity;
 import com.boot.security.server.service.IpsInfoService;
 import com.boot.security.server.utils.SysUtils;
 
+import jxl.common.Logger;
+
 @Service
 public class IpsInfoServiceImpl implements IpsInfoService {
+	
+	private Logger LOGGER = Logger.getLogger(IpsInfoServiceImpl.class);
 	@Autowired
 	private IpsInfoDao ipsInfoDao;
 	@Override
-	public IpsInfoEntity getBySysname() {
-		return ipsInfoDao.getBySysname(SysUtils.getInetAddress().getHostName());
+	public List<IpsInfoEntity> getBySysname() {
+		List<IpsInfoEntity> bySysname = ipsInfoDao.getBySysname(SysUtils.getInetAddress().getHostName());
+		for (int i = 0; i < bySysname.size(); i++) {
+			LOGGER.info(bySysname.get(i).toString());
+		}
+		return bySysname;
 	}
 
 }
